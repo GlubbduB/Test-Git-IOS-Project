a031d1159caf3fd902f7be84c41797390b958338
GlubbduB <sjdtielemans@yahoo.fr>
2014-05-18 19:22:28 +0200

    added a test class

---
+++ Git Test/STTest.h
@@ -0,0 +1,13 @@
+//
+//  STTest.h
+//  Git Test
+//
+//  Created by Stéphane Tielemans on 18/05/14.
+//  Copyright (c) 2014 Stéphane Tielemans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STTest : NSObject
+
+@end
---
+++ Git Test/STTest.m
@@ -0,0 +1,13 @@
+//
+//  STTest.m
+//  Git Test
+//
+//  Created by Stéphane Tielemans on 18/05/14.
+//  Copyright (c) 2014 Stéphane Tielemans. All rights reserved.
+//
+
+#import "STTest.h"
+
+@implementation STTest
+
+@end
