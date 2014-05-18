f1a4db0cbd3e102e002787e4c3ff473d48050bdf
Stéphane Tielemans <stephanetielemans@MacBook-Pro-de-Stephane.local>
2014-05-18 17:18:27 +0200

    Initial Commit

---
+++ Git Test/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  Git Test
+//
+//  Created by Stéphane Tielemans on 18/05/14.
+//  Copyright (c) 2014 Stéphane Tielemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
